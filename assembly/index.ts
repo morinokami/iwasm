bf4ebe51c42f85b1c2abc5bf80ea8857bcff65c2
Shinya Fujino <shf0811@gmail.com>
2021-05-03 00:58:17 +0900

    Using the __getString Function

--- assembly/index.ts
+++ assembly/index.ts
@@ -4,3 +4,16 @@ export function minusOne(n: i32): i32 {
   log(n);
   return n - 1;
 }
+
+export function fizzbuzz(n: i32): string | null {
+  if (n % 15 === 0) {
+    return "fizzbuzz";
+  }
+  if (n % 3 === 0) {
+    return "fizz";
+  }
+  if (n % 5 === 0) {
+    return "buzz";
+  }
+  return null;
+}
--- js/loader.js
+++ js/loader.js
@@ -17,14 +17,11 @@ class WasmLoader {
   async wasm(path, imports = this._imports) {
     console.log(`Fetching ${path}`);
 
-    if (!WebAssembly.instantiateStreaming) {
+    if (!loader.instantiateStreaming) {
       return this.wasmFallback(path, imports);
     }
 
-    const { instance } = await WebAssembly.instantiateStreaming(
-      fetch(path),
-      imports
-    );
+    const instance = await loader.instantiateStreaming(fetch(path), imports);
 
     return instance.exports;
   }
@@ -34,7 +31,7 @@ class WasmLoader {
 
     const response = await fetch(path);
     const bytes = await response.arrayBuffer();
-    const { instance } = await WebAssembly.instantiate(bytes, imports);
+    const instance = await loader.instantiate(bytes, imports);
 
     return instance.exports;
   }
